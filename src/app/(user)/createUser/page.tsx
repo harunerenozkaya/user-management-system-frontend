6d3410d1f7b1b67edc987cc93cd329306bf61bb4
Harun Eren Özkaya <harunerenozkaya@gmail.com>
2024-01-06 12:40:40 +0300

    create add and edit user pages

--- src/app/(user)/createUser/page.tsx
+++ src/app/(user)/createUser/page.tsx
@@ -1,7 +1,10 @@
+import UserDetailForm from "@/components/userDetailForm/userDetailForm";
+
 export default function CreateUser() {
   return (
     <div>
       <h1>Create a new user</h1>
+      <UserDetailForm actionType="add"/>
     </div>
   );
 }
--- src/app/(user)/editUser/page.tsx
+++ src/app/(user)/editUser/page.tsx
@@ -1,7 +1,14 @@
+'use client';
+import UserDetailForm from "@/components/userDetailForm/userDetailForm";
+import { useSearchParams } from "next/navigation";
+
 export default function EditUser() {
+    const searchParams = useSearchParams();
+    const id = searchParams.get("id");
     return (
       <div>
         <h1>Edit user</h1>
+        <UserDetailForm actionType="edit"/>
       </div>
     );
   }
